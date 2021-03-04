91d230faacd1c019a8a7635f6e9d795d8891ec81
yuceltoluyag <ytoluyag@gmail.com>
2021-03-05 01:38:03 +0300

    Fantom

--- tg.py
+++ tg.py
@@ -8,22 +8,26 @@ chat_id = environ['YOUR_CHAT_ID']
 time_interval = 3600  # in seconds
 
 
-def get_crypto_price(sfppricelist, juldpricelist, leadpricelist, pundixpricelist):
+def get_crypto_price(
+    sfppricelist, juldpricelist, leadpricelist, pundixpricelist, ftmpricelist
+):
 
     geckoAPI = CoinGeckoAPI()
     response = geckoAPI.get_price(
-        ids=['safepal', 'julswap', 'lead-token', 'pundi-x'],
+        ids=['safepal', 'julswap', 'lead-token', 'pundi-x', 'fantom'],
         vs_currencies="usd",
     )
     sfppricelist.append(response["safepal"]["usd"])
     leadpricelist.append(response["julswap"]["usd"])
     juldpricelist.append(response["lead-token"]["usd"])
     pundixpricelist.append(response["pundi-x"]["usd"])
+    ftmpricelist.append(response["fantom"]["usd"])
 
-    return sfppricelist, leadpricelist, juldpricelist, pundixpricelist
+    return sfppricelist, leadpricelist, juldpricelist, pundixpricelist, ftmpricelist
 
 
 # fn to send_message through telegram
+# https://api.telegram.org/bot1402190466:AAFx-_-ysIYulpQQsrhBoLzqg8Ui32p8OHw/sendMessage?chat_id=-1001392588491&text=sa
 def send_message(chat_id, msg):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&text={msg}"
 
@@ -43,6 +47,7 @@ def format_msg(pricelisting):
         f'\nJULD: {pricelisting[1][0]}$ -> {pricelisting[1][1]}$ = {difflisting[1]}$!'
         f'\nLEAD: {pricelisting[2][0]}$ -> {pricelisting[2][1]}$ = {difflisting[2]}$!'
         f'\nNPXS: {pricelisting[3][0]}$ -> {pricelisting[3][1]}$ = {difflisting[3]}$!'
+        f'\nFTM: {pricelisting[4][0]}$ -> {pricelisting[4][1]}$ = {difflisting[4]}$!'
     )
 
     return msg
@@ -53,11 +58,12 @@ def main():
     juldpricelist = []
     leadpricelist = []
     pundixpricelist = []
+    ftmpricelist = []
 
     # infinite loop
     while True:
         pricelistings = get_crypto_price(
-            sfppricelist, juldpricelist, leadpricelist, pundixpricelist
+            sfppricelist, juldpricelist, leadpricelist, pundixpricelist, ftmpricelist
         )
 
         # send last 6 btc price
